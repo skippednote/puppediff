52f44f5710380ff922a8ec9afda63af4fc8e2402
Bassam Ismail <skippednote@gmail.com>
2018-03-30 12:54:03 +0530

    (feat): Add support for breakpoints.

--- diff.js
+++ diff.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 const looksSame = require('looks-same');
-const { baseUrl, pages } = require('./pages.json');
+const { baseUrl, pages, breakpoints } = require('./pages.json');
 
-pages.forEach(({ name, url }) => {
-  var updated = `./screenshots/${name}.png`;
-  var baseline = `./screenshots/${name}-baseline.png`;
-  var diff = `./screenshots/${name}-diff.png`;
+breakpoints.forEach(({ width }) => {
+  pages.forEach(({ name, url }) => {
+    var updated = `./screenshots/${name}-${width}.png`;
+    var baseline = `./screenshots/${name}-${width}-baseline.png`;
+    var diff = `./screenshots/${name}-${width}-diff.png`;
 
-  if (fs.existsSync(baseline) && fs.existsSync(updated)) {
-    looksSame.createDiff(
-      {
-        reference: baseline,
-        current: updated,
-        diff: diff,
-        highlightColor: '#ff00ff',
-        strict: false,
-        tolerance: 2.5
-      },
-      function(error) {
-        if (error) console.warn(error);
-      }
-    );
-  } else {
-    console.log(`Missing the baseline or updated image for "${name}".`);
-  }
+    if (fs.existsSync(baseline) && fs.existsSync(updated)) {
+      looksSame.createDiff(
+        {
+          reference: baseline,
+          current: updated,
+          diff: diff,
+          highlightColor: '#ff00ff',
+          strict: false,
+          tolerance: 5
+        },
+        function(error) {
+          if (error) console.warn(error);
+        }
+      );
+    } else {
+      console.log(`Missing the baseline or updated image for "${name}".`);
+    }
+  });
 });
--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const baseline = process.argv[2];
-const { baseUrl, pages } = require('./pages.json');
+const { baseUrl, pages, breakpoints } = require('./pages.json');
 const dir = './screenshots';
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
 
-const screenshot = async (name, url) => {
+const screenshot = async (name, url, { width, height }) => {
   const browser = await puppeteer.launch({
     headless: true,
     ignoreHTTPSErrors: true,
-    slowMo: 1000
+    slowMo: 1000,
+    timeout: 0
   });
   const page = await browser.newPage();
   await page.goto(baseUrl + url);
-  await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 1 });
+  await page.setViewport({
+    width: width,
+    height: height,
+    deviceScaleFactor: 1
+  });
   // await page.click('.cc_btn_accept_all');
 
   await page.screenshot({
     path: baseline
-      ? `./screenshots/${name}-baseline.png`
-      : `./screenshots/${name}.png`,
+      ? `./screenshots/${name}-${width}-baseline.png`
+      : `./screenshots/${name}-${width}.png`,
     fullPage: true
   });
   await browser.close();
 };
 
-pages.forEach(({ name, url }) => {
-  screenshot(name, url);
+breakpoints.forEach(breakpoint => {
+  pages.forEach(({ name, url }) => {
+    screenshot(name, url, breakpoint);
+  });
 });
